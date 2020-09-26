2de7b7073626eabc46ec1f1d54431190aa01f3b8
rhjaf <rhjafrooz@protonmail.com>
2020-09-26 19:30:05 +0330

    Color Panel

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,12 @@ import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 import { connect } from "react-redux";
 
-const App = ({currentUser,currentChannel,isPrivateChannel,userPosts}) => (
-  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
-    <ColorPanel />
-    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser}/>
+
+
+const App = ({currentUser,currentChannel,isPrivateChannel,userPosts,primaryColor,secondaryColor}) => (
+  <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
+    <ColorPanel currentUser={ currentUser } key={currentUser && currentUser.name} />
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} primaryColor={primaryColor}/>
 
     <Grid.Column style={{ marginLeft: 320 }}>
       <Messages key={currentChannel && currentChannel.id}
@@ -20,7 +22,7 @@ const App = ({currentUser,currentChannel,isPrivateChannel,userPosts}) => (
                 isPrivateChannel={isPrivateChannel}/>
     </Grid.Column>
     <Grid.Column width={4}>
-      <MetaPanel currentChannel={currentChannel} key={currentChannel && currentChannel.id}
+      <MetaPanel currentChannel={currentChannel} key={currentChannel && currentChannel.name}
                  isPrivateChannel={isPrivateChannel} userPosts={userPosts}/>
     </Grid.Column>
   </Grid>
@@ -30,7 +32,9 @@ const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
     currentChannel : state.channel.currentChannel,
     isPrivateChannel : state.channel.isPrivateChannel,
-    userPosts : state.channel.userPosts
+    userPosts : state.channel.userPosts,
+    primaryColor: state.colors.primaryColor,
+    secondaryColor: state.colors.secondaryColor
 });
 
 export default connect(mapStateToProps)(App);
